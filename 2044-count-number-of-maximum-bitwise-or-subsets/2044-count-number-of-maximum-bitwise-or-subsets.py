e39534b1f840293d2b50c0d394d3da43e927b263
suojae <suojae.developer@gmail.com>
2024-10-18 20:41:43 +0900

    Time: 413 ms (48.13%), Space: 16.5 MB (87.92%) - LeetHub

---
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.py
@@ -0,0 +1,26 @@
+from itertools import combinations
+
+class Solution:
+    def countMaxOrSubsets(self, nums):
+        max_or = 0
+        count = 0
+
+        for num in nums:
+            max_or |= num 
+
+        def bitwise_or(subset):
+            result = 0
+            for num in subset:
+                result |= num
+            return result
+
+        n = len(nums)
+
+        for r in range(1, n + 1):
+            for subset in combinations(nums, r):
+                if bitwise_or(subset) == max_or:
+                    count += 1
+
+        return count 
+
+sol = Solution()
