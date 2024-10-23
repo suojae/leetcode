30e88595a65515d5e3779398856eba1217c7d744
suojae <suojae.developer@gmail.com>
2024-10-23 15:21:23 +0900

    Time: 111 ms (97.9%), Space: 57.4 MB (81.82%) - LeetHub

---
+++ 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.py
@@ -0,0 +1,30 @@
+# 틀린문제
+# 모범답안
+
+from collections import deque
+class Solution:
+    def replaceValueInTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        
+        pq = deque()
+        pq.append((root.val, root))
+        
+        while pq:
+            n = len(pq)
+            
+            levelSum = 0
+            for localSum, node in pq:
+                levelSum += node.val
+                
+            for i in range(n):
+                localSum, node = pq.popleft()
+                
+                childSum = 0
+                if node.left: childSum += node.left.val
+                if node.right: childSum += node.right.val
+                
+                if node.left: pq.append((childSum, node.left))
+                if node.right: pq.append((childSum, node.right))
+                   
+                node.val = levelSum - localSum
+                 
+        return root
