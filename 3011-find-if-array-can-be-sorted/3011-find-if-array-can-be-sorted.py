a6821f87c7b09bb810d53227f4213ab4fac29b6e
suojae <suojae.developer@gmail.com>
2024-11-06 22:06:11 +0900

    Time: 106 ms (6.36%), Space: 16.7 MB (43.17%) - LeetHub

--- 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.py
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.py
@@ -1,19 +1,23 @@
 from typing import List
-from collections import defaultdict
 
 class Solution:
     def canSortArray(self, nums: List[int]) -> bool:
-        # 이미 정렬된 경우 True 반환
-        if nums == sorted(nums):
-            return True
+        n = len(nums)
 
-        groups = defaultdict(list)
-        for num in nums:
-            count = bin(num).count('1')  # 이진수에서 '1'의 개수를 세기
-            groups[count].append(num)
+        # 각 숫자의 '1' 개수를 계산하여 리스트에 저장
+        one_counts = [bin(num).count('1') for num in nums]
+        
+        # 인접한 요소들을 비교하여 교환 가능한지 확인
+        for i in range(n):
+            for j in range(n - 1):
+                if nums[j] > nums[j + 1]:
+                    if one_counts[j] == one_counts[j + 1]:
+                        # '1'의 개수가 같으면 교환
+                        nums[j], nums[j + 1] = nums[j + 1], nums[j]
+                        one_counts[j], one_counts[j + 1] = one_counts[j + 1], one_counts[j]
+                    else:
+                        # '1'의 개수가 다르면 교환 불가
+                        continue
 
-        sorted_nums = []
-        for count in sorted(groups.keys()):
-            sorted_nums.extend(sorted(groups[count]))
-
-        return sorted_nums == sorted(nums)
+        # 정렬된 결과와 원본을 비교하여 확인
+        return nums == sorted(nums)
