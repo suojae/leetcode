ffed8cbeb02ad66211f45fc760fee488b55861b9
suojae <suojae.developer@gmail.com>
2024-11-06 22:08:37 +0900

    Time: 118 ms (5.45%), Space: 16.7 MB (43.17%) - LeetHub

--- 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.py
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.py
@@ -21,3 +21,31 @@ class Solution:
 
         # 정렬된 결과와 원본을 비교하여 확인
         return nums == sorted(nums)
+
+
+# 모범답안
+class Solution2:
+    def canSortArray(self, nums):
+        num_of_set_bits = bin(nums[0]).count("1")
+        max_of_segment = nums[0]
+        min_of_segment = nums[0]
+
+        max_of_prev_segment = float("-inf")
+
+        for i in range(1, len(nums)):
+            if bin(nums[i]).count("1") == num_of_set_bits:
+                max_of_segment = max(max_of_segment, nums[i])
+                min_of_segment = min(min_of_segment, nums[i])
+            else:  
+                if min_of_segment < max_of_prev_segment:
+                    return False
+
+                max_of_prev_segment = max_of_segment
+                max_of_segment = nums[i]
+                min_of_segment = nums[i]
+                num_of_set_bits = bin(nums[i]).count("1")
+
+        if min_of_segment < max_of_prev_segment:
+            return False
+
+        return True
