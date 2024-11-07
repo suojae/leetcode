99eee179d1d68c0c8e42bd8d2748d78804496943
suojae <suojae.developer@gmail.com>
2024-11-07 15:08:32 +0900

    Time: 451 ms (83.75%), Space: 26.8 MB (60.3%) - LeetHub

--- 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.py
+++ 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.py
@@ -4,14 +4,13 @@ from typing import List
 class Solution:
     def largestCombination(self, candidates: List[int]) -> int:
         candidates_count = len(candidates)
-        result_array = []
+        max_count = 0
 
-        for r in range(1, candidates_count + 1):
-            combination_array = itertools.combinations(candidates, r)
-            for num_array in combination_array:
-                temp_result = 255  # 8비트의 모든 비트가 1인 값
-                for num in num_array:
-                    temp_result &= num
-                result_array.append(temp_result)
+        for i in range(24, -1, -1):  
+            count = 0
+            for num in candidates:
+                if num & (1 << i):  
+                    count += 1
+            max_count = max(max_count, count)
 
-        return max(result_array)
+        return max_count
