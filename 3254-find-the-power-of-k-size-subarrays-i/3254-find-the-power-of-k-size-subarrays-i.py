80621fd4bcfd4941488d7336650c1b16d6ea3d35
suojae <suojae.developer@gmail.com>
2024-11-16 15:03:54 +0900

    Time: 87 ms (12.77%), Space: 16.6 MB (86.53%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.py
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.py
@@ -3,7 +3,7 @@ class Solution:
         ans_arr = []
         
         for start in range(len(nums) - k + 1):  
-            tmp_arr = nums[start:start + k]    #
+            tmp_arr = nums[start:start + k]    
             
             
             if all(tmp_arr[i] + 1 == tmp_arr[i + 1] for i in range(len(tmp_arr) - 1)):
@@ -12,3 +12,5 @@ class Solution:
                 ans_arr.append(-1)           # 연속적이지 않으면 -1 추가
                 
         return ans_arr
+
+
