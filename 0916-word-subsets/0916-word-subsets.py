52523b5e5d764584df202f27def5c1c50171c629
suojae <suojae.developer@gmail.com>
2025-01-10 21:59:21 +0900

    Time: 339 ms (70.12%), Space: 21.2 MB (32.67%) - LeetHub

--- 0916-word-subsets/0916-word-subsets.py
+++ 0916-word-subsets/0916-word-subsets.py
@@ -4,21 +4,21 @@ from collections import Counter
 class Solution:
     def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
         answer = []
-
-        # words2의 문자 개수 중 최댓값을 저장
+        
+        # words2 내부 단어들의 문자 빈도 중 최댓값 계산
         max_freq = Counter()
-        for word2 in words2:
-            word_count = Counter(word2)
+        for word in words2:
+            word_count = Counter(word)
             for char, freq in word_count.items():
                 max_freq[char] = max(max_freq[char], freq)
 
         for word1 in words1:
             word_count = Counter(word1)
-            count = 0  # 조건을 만족하는 개수
+            count = 0  
             for char, freq in max_freq.items():
                 if word_count[char] >= freq:
                     count += 1
-            if count == len(max_freq):  # 모든 조건을 만족하는 경우
+            if count == len(max_freq): 
                 answer.append(word1)
 
         return answer
