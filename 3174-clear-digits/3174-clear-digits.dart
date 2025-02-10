96cb2e3691ddee74f0580a9a3b33ce1a05351a12
suojae <suojae.developer@gmail.com>
2025-02-10 17:57:16 +0900

    Time: 8 ms (25%), Space: 149 MB (50%) - LeetHub

--- 3174-clear-digits/3174-clear-digits.dart
+++ 3174-clear-digits/3174-clear-digits.dart
@@ -1,23 +1,6 @@
 class Solution {
-  String clearDigits(String s) {
-    return _clearDigitsRec(s);
-  }
-
-  String _clearDigitsRec(String s) {
-    final digits = '0123456789';
-
-    // 숫자가 포함되지 않으면 그대로 반환
-    if (!s.split('').any(digits.contains)) return s;
-
-    final filtered = s.split('').fold<List<String>>([], (acc, char) {
-      if (acc.isEmpty || !digits.contains(char)) {
-        acc.add(char);
-      } else {
-        acc.removeLast();
-      }
-      return acc;
-    }).join();
-
-    return _clearDigitsRec(filtered);
-  }
+  String clearDigits(String s) => s.split('').fold('', (acc, ch) =>
+      int.tryParse(ch) != null ? acc.substring(0, acc.length - 1) : acc + ch
+    );
+  
 }
