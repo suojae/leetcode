253581da2fc3662102753d525231b91b32850c18
suojae <suojae.developer@gmail.com>
2025-02-10 17:52:12 +0900

    Time: 6 ms (50%), Space: 148.2 MB (75%) - LeetHub

--- 3174-clear-digits/3174-clear-digits.dart
+++ 3174-clear-digits/3174-clear-digits.dart
@@ -1,22 +1,23 @@
 class Solution {
-String clearDigits(String s) {
-  if (!s.split('').any((c) => '0123456789'.contains(c))) return s; 
+  String clearDigits(String s) {
+    return _clearDigitsRec(s);
+  }
 
-  final digitIndex = s.split('').indexWhere((c) => '0123456789'.contains(c));
+  String _clearDigitsRec(String s) {
+    final digits = '0123456789';
 
-  final leftIndex = s.substring(0, digitIndex)
-      .split('')
-      .lastIndexWhere((c) => !'0123456789'.contains(c));
+    // 숫자가 포함되지 않으면 그대로 반환
+    if (!s.split('').any(digits.contains)) return s;
 
-  // 숫자와 가장 가까운 왼쪽 문자를 제거
-  final filtered = s.split('')
-      .asMap()
-      .entries
-      .where((entry) => entry.key != digitIndex && entry.key != leftIndex)
-      .map((entry) => entry.value)
-      .join();
+    final filtered = s.split('').fold<List<String>>([], (acc, char) {
+      if (acc.isEmpty || !digits.contains(char)) {
+        acc.add(char);
+      } else {
+        acc.removeLast();
+      }
+      return acc;
+    }).join();
 
-  return clearDigits(filtered); // 재귀 호출하여 계속 반복
-}
-
+    return _clearDigitsRec(filtered);
+  }
 }
