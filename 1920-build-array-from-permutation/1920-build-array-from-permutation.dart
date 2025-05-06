993eaec5dd6ade7be78ce91c212721d66d7f9769
suojae <suojae.developer@gmail.com>
2025-05-06 20:05:22 +0900

    Time: 0 ms (100%), Space: 152.8 MB (13.51%) - LeetHub

---
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.dart
@@ -0,0 +1,12 @@
+class Solution {
+  List<int> buildArray(List<int> nums) {
+
+    List<int> ans = [];
+
+    for (int i = 0; i < nums.length; i++) {
+        ans.add(nums[nums[i]]);
+    }
+
+    return ans;
+  }
+}
